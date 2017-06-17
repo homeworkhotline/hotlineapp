47057bcebf78fff3691ab7dec277eb4a00b1fbbd
Homework Hotline <homeworkhotlinenashville@gmail.com>
2017-06-17 17:36:45 -0400

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
 # SMTP settings for gmail
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
+ :domain               => "gmail.com",
  :port                 => 587,
  :user_name            => ENV['gmail_username'],
  :password             => ENV['gmail_password'],
