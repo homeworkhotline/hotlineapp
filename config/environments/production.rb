0553a4dd8aee1f4598f21015d6724bf0ef53b88d
Homework Hotline <homeworkhotlinenashville@gmail.com>
2017-06-26 12:55:29 -0500

    test ac

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,8 +50,8 @@ config.action_mailer.smtp_settings = {
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+   config.action_cable.url = 'wss:http://app.homeworkhotline.info/'
+   config.action_cable.allowed_request_origins = [ 'http://app.homeworkhotline.info/', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
